5a1fa436a1f1559dec384b649005f28bcf6effd9
Ihor <fedonn@ukr.net>
2023-09-10 10:51:45 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/GlobalStyle'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
-import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<PersistGate loading={null} persistor={persistor}>
 		<Provider store={store}>
-			<HashRouter basename='rentcar'>
+			<BrowserRouter basename='rentcar'>
 				<GlobalStyles />
 				<App />
-			</HashRouter>
+			</BrowserRouter>
 		</Provider>
 	</PersistGate>
 )
