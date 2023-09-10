ec495702984fc49007efa436b6cdd1865470e417
Ihor <fedonn@ukr.net>
2023-09-10 10:44:09 +0300

    update router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/GlobalStyle'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
